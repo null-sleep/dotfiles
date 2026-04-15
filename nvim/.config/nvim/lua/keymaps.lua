f9bfa872920c46bfc0cc37f972e9a320810e384d
Dhruv Jauhar <dhruvjhr@gmail.com>
2026-04-15 17:02:44 -0400

    Add <leader>s/ keymap for current buffer fuzzy find

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -4,4 +4,5 @@ vim.keymap.set('n', '<leader>sf', builtin.find_files,  { desc = 'Search: Files' 
 vim.keymap.set('n', '<leader>sg', builtin.live_grep,   { desc = 'Search: Grep' })
 vim.keymap.set('n', '<leader>sb', builtin.buffers,     { desc = 'Search: Buffers' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags,   { desc = 'Search: Help tags' })
-vim.keymap.set('n', '<leader>sr', builtin.resume,      { desc = 'Search: Resume last' })
+vim.keymap.set('n', '<leader>sr', builtin.resume,                      { desc = 'Search: Resume last' })
+vim.keymap.set('n', '<leader>s/', builtin.current_buffer_fuzzy_find,   { desc = 'Search: Current buffer' })
