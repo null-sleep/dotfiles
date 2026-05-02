92067d533fcce3f928d4d0e351d4a62a44c59262
Dhruv Jauhar <dhruvjhr@gmail.com>
2026-05-02 13:21:51 -0400

    Exempt cursortab.nvim from orphaned plugin check
    Experimenting with it on another branch; exclude it so it doesn't
    trigger warnings or get accidentally deleted on main.

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -50,8 +50,9 @@ vim.pack.add(vim.list_extend({
 
 -- Warn about orphaned plugins (on disk but not in vim.pack.add list)
 vim.defer_fn(function()
+  local exceptions = { 'cursortab.nvim' }
   local orphans = vim.iter(vim.pack.get(nil, { info = false }))
-    :filter(function(x) return not x.active end)
+    :filter(function(x) return not x.active and not vim.tbl_contains(exceptions, x.spec.name) end)
     :map(function(x) return x.spec.name end)
     :totable()
   if #orphans > 0 then
